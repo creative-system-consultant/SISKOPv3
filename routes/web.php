24d4cb1f63ecb94b3fb268d7d0d1748fbdc92924
Amirul Shahiran <shahirankamal74@gmail.com>
2022-06-30 17:03:44 +0800

    Add route web

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use App\Http\Livewire\admin\maintenance\education\ListEducation;
 use App\Http\Livewire\Admin\Maintenance\gender\gender;
 use App\Http\Livewire\Admin\Maintenance\gender\gendercreate;
 use App\Http\Livewire\Admin\Maintenance\gender\genderedit;
+use App\Http\Livewire\Admin\maintenance\marital\CreateMarital;
+use App\Http\Livewire\Admin\maintenance\marital\EditMarital;
+use App\Http\Livewire\Admin\maintenance\marital\ListMarital;
 use App\Http\Livewire\admin\maintenance\race\CreateRace;
 use App\Http\Livewire\admin\maintenance\race\EditRace;
 use App\Http\Livewire\admin\maintenance\race\ListRace;
@@ -28,6 +31,9 @@ use App\Http\Livewire\Admin\Maintenance\state\stateedit;
 use App\Http\Livewire\admin\maintenance\title\CreateTitle;
 use App\Http\Livewire\admin\maintenance\title\EditTitle;
 use App\Http\Livewire\admin\maintenance\title\ListTitle;
+use App\Http\Livewire\Admin\maintenance\relationship\CreateRelationship;
+use App\Http\Livewire\Admin\maintenance\relationship\EditRelationship;
+use App\Http\Livewire\Admin\maintenance\relationship\ListRelationship;
 use App\Http\Livewire\Auth\Login;
 use App\Http\Livewire\Auth\Passwords\Confirm;
 use App\Http\Livewire\Auth\Passwords\Email;
@@ -46,6 +52,8 @@ use App\Http\Livewire\Page\Home;
 use App\Http\Livewire\Page\Profile\Index;
 use App\Http\Livewire\Page\Reporting\ListReport;
 use App\Http\Livewire\Page\Reporting\UserReporting;
+use App\Http\Livewire\customers\SearchCustomer;
+use App\Http\Livewire\customers\EditCustomer;
 
 /*
 |--------------------------------------------------------------------------
@@ -81,6 +89,7 @@ Route::middleware('auth')->group(function () {
 
     //profile
     Route::get('profile', Index::class)->name('profile');
+    Route::get('Index', Index::class)->name('Index');
 
 
     //------------------------admin------------------------------//
@@ -142,6 +151,22 @@ Route::middleware('auth')->group(function () {
     Route::get('gendercreate', gendercreate::class)->name('gendercreate');
     Route::get('gendernedit/{id}', genderedit::class)->name('genderedit');
     Route::get('delete/{id}', gender::class)->name('delete');
+
+    //Admin > Maintenance > Relationship
+    Route::get('relationshiplist', ListRelationship::class)->name('relationship.list');
+    Route::get('relationshipcreate', CreateRelationship::class)->name('relationship.create');
+    Route::get('relationshipedit/{id}', EditRelationship::class)->name('relationship.edit');
+
+    //Admin > Maintenance > Marital
+    Route::get('maritallist', ListMarital::class)->name('marital.list');
+    Route::get('maritalcreate', CreateMarital::class)->name('marital.create');
+    Route::get('maritaledit/{id}', EditMarital::class)->name('marital.edit');
+
+    //customers
+    //customers
+    Route::get('searchcustomer', SearchCustomer::class)->name('searchcustomer');
+    Route::get('edit/{id}', EditCustomer::class)->name('edit');
+
 
     //------------------------Reporting------------------------------//
     //Report > Sample
