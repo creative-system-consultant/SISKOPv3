925894931ffa40ddfc5c71e4365ca051df3a1216
Danish Aiman <danishaiman2811@gmail.com>
2022-07-12 13:30:51 +0800

    Add two option on Apply Sell /Exchange Share
    - Create dropdown menu for to member option and 
       to corporation option
    
    - Bugfix sell share to corporation option
    
    - Add confirmation message when submit
    
    - Add checking if member icno is exist in 
       CIF.customer at to member option

--- app/Http/Livewire/Page/Application/ApplySellExchangeShare/Apply_Sell_ExchangeShare.php
+++ app/Http/Livewire/Page/Application/ApplySellExchangeShare/Apply_Sell_ExchangeShare.php
@@ -11,60 +11,133 @@ class Apply_Sell_ExchangeShare extends Component
 {
     public Customer $cust;
     public $share_apply;
+    public $share_type;
+    public $mbr_icno;
     public $bank_name;
+    public $bank_acct;
     public $bank_account;
+    public $bank_code;
     public $banks;
-    public $share;
+    
+    //Need protected $listerners to run the Livewire.emit event
+    protected $listeners = ['submit'];      
 
     protected $rules = [
         'cust.name'       => 'required',
         'cust.icno'       => 'required',
         'cust.share'      => 'required',
         'share_apply'     => 'required|numeric',
-        'bank_name'       => 'required',
-        'bank_account'    => 'required',
+        'share_type'      => 'required',
+        'bank_name'       => 'required_if:share_type,==,coop',
+        'bank_account'    => 'required_if:share_type,==,coop',
+        'mbr_icno'        => 'required_if:share_type,==,mbr',
+        'bank_code'       => 'required_if:share_type,==,mbr',
+        'bank_acct'       => 'required_if:share_type,==,mbr',
     ];
     
     protected $messages = [
+        'mbr_icno.required_if'       => ':attribute field is required',   
         'share_apply.required'       => ':attribute field is required',
-        'bank_name.required'         => ':attribute field is required',
-        'bank_account.required'      => ':attribute field is required'
+        'share_apply.numeric'        => ':attribute field must be a number',
+        'share_type.required'        => ':attribute is required',
+        'bank_name.required_if'      => ':attribute field is required',
+        'bank_code.required_if'      => ':attribute field is required',
+        'bank_account.required_if'   => ':attribute field is required',
+        'bank_acct.required_if'      => ':attribute field is required',
     ];
 
     protected $validationAttributes = [
+        'mbr_icno'        => 'Member IC No.',
         'share_apply'     => 'Reimbursement of Share Capital applied',
+        'share_type'      => 'Types of Share Reimbursement',
         'bank_name'       => 'Bank',
-        'bank_account'    => 'Account Bank No.'
+        'bank_code'       => 'Bank',
+        'bank_account'    => 'Account Bank No.',
+        'bank_acct'       => 'Account Bank No.'
     ];
+    
 
     public function submit()
     {
-        $this->validate();
-        
         $user = auth()->user();
-        $customer = Customer::where('icno', $user->icno)->first();
+        $customer = new Customer;        
 
-        $share = share::create([
-            'coop_id'      => $customer->coop_id,
-            'cust_id'      => $customer->id,
-            'amt_before'   => $this->cust['share'] ??= '0',
-            'apply_amt'    => $this->share_apply,
-            'approved_amt' => NULL,
-            'bank_code'    => $this->bank_name,
-            'bank_account' => $this->bank_account,
-            'direction'    => 'sell',
-            'flag'         => '1',
-            'step'         => '1',
-        ]);
+        $this->validate();
 
-        session()->flash('message', 'Share Reimbursement Application Successfully Send');
-        session()->flash('success');
-        session()->flash('title');
+        if ($this->share_apply <= 0) {
+            session()->flash('message', 'Application must be more than RM0');
+            session()->flash('warning');
+            session()->flash('title');
 
-        return redirect()->route('share.sell');  
+            return redirect()->route('share.sell');
+        }
+        elseif($this->share_apply > $this->cust['share']){
+            session()->flash('message', 'Application must be less than RM'.$this->cust['share']);
+            session()->flash('warning');
+            session()->flash('title');
+
+            return redirect()->route('share.sell');
+        }
+
+        if ($this->share_type == 'coop') {
+            $cust = $customer->where('icno', $user->icno)->first();
+            $share = share::where([['cust_id', $cust->id], ['flag', 0], ['step', 0], ['direction', 'sell']])->first();
+
+            $share->update([
+                'amt_before'   => $this->cust['share'],
+                'apply_amt'    => $this->share_apply,
+                'approved_amt' => NULL,
+                'bank_code'    => $this->bank_name,
+                'bank_account' => $this->bank_account,
+                'flag'         => '1',
+                'step'         => '1',
+                'created_by'   => strtoupper($cust->name),
+            ]);
+    
+            session()->flash('message', 'Share Reimbursement Application Successfully Send');
+            session()->flash('success');
+            session()->flash('title');
+    
+            return redirect('home');  
+        }
+        elseif ($this->share_type == 'mbr') {
+            $cust = $customer->where('icno', $user->icno)->first();
+            $cust_member = $customer->where([['icno', $this->mbr_icno],['icno' ,'<>', $user->icno]])->first();
+            $share = share::where([['cust_id', $cust->id], ['flag', 0], ['step', 0], ['direction', 'exchange']])->first();
+
+            $share->update([
+                'amt_before'   => $this->cust['share'],
+                'apply_amt'    => $this->share_apply,
+                'approved_amt' => NULL,
+                'bank_code'    => $this->bank_code,
+                'bank_account' => $this->bank_acct,
+                'exc_cust_id'  => $cust_member->id,
+                'flag'         => '1',
+                'step'         => '1',
+                'created_by'   => strtoupper($cust->name),                
+            ]);
+
+            session()->flash('message', 'Share Reimbursement Application Successfully Send');
+            session()->flash('success');
+            session()->flash('title');
+    
+            return redirect('home');  
+        }
+        else{
+            //
+        }
+        
     }
 
-    public function restricApply($id)
+    public function alertConfirm()
+    {
+        $this->dispatchBrowserEvent('swal:confirm', [
+            'type'      => 'warning',  
+            'text'      => 'Are you sure you want to apply for share reimbursement?',
+        ]);   
+    }
+
+    public function restricApplySell($id)
     {
         $share = share::where([['cust_id', $id ], ['flag', 1], ['step', 1], ['direction', 'sell']])->first();
 
@@ -74,6 +147,86 @@ class Apply_Sell_ExchangeShare extends Component
             session()->flash('title');
     
             return redirect()->route('home');                                
+        }    
+    }
+
+    public function restricApplyExc($id)
+    {
+        $share = share::where([['cust_id', $id ], ['flag', 1], ['step', 1], ['direction', 'exchange']])->first();
+
+        if ($share != null) {
+            session()->flash('message', 'Share reimbursement application is been processed. If you want to make another application, please wait until the application is processed');
+            session()->flash('info');
+            session()->flash('title');
+    
+            return redirect()->route('home');                                
+        }    
+    }
+
+
+    public function contApplyMember($cust_id)
+    {
+        $share = share::where('cust_id', $cust_id)->firstOrCreate([
+            'coop_id'     => $this->cust->coop_id, 
+            'cust_id'     => $this->cust->id, 
+            'amt_before'  => $this->cust->share,
+            'direction'   => 'exchange',
+            'flag'        => 0, 
+            'step'        => 0
+            ], [
+                'apply_amt'   => '0.00',  
+        ]);        
+
+        $this->share_apply  = $share?->apply_amt;
+        $this->bank_acct    = $share?->bank_account;
+        $this->bank_code    = $share?->bank_code;  
+    }
+
+    public function contApplyCoop($cust_id)
+    {
+        $share = share::where('cust_id', $cust_id)->firstOrCreate([
+            'coop_id'     => $this->cust->coop_id, 
+            'cust_id'     => $this->cust->id, 
+            'amt_before'  => $this->cust->share,
+            'direction'   => 'sell',
+            'flag'        => 0, 
+            'step'        => 0
+            ], [
+                'apply_amt'   => '0.00',  
+        ]);
+
+        $this->share_apply  = $share?->apply_amt;
+        $this->bank_account = $share?->bank_account;
+        $this->bank_name    = $share?->bank_code ;
+    }
+
+    public function updated()
+    {
+        $user = auth()->user();       
+        $customer = Customer::where([['icno', $this->mbr_icno],['icno' ,'<>', $user->icno]])->first();
+
+        if (strlen($this->mbr_icno) < 12) {
+            $this->mbr_name = ''; 
+            return;
+        }
+
+        if (strlen($this->mbr_icno) > 12) {
+            $this->mbr_name = ''; 
+            return;
+        }
+
+        if ($customer == NULL) {
+            $this->dispatchBrowserEvent('swal',[
+                'title' => '',
+                'text'  => 'Customer Not Found',
+                'icon'  => 'warning',
+                'showConfirmButton' => false,
+                'timer' => 1500,
+            ]);
+            $this->mbr_name = '';    
+        }
+        else{
+            $this->mbr_name = $customer->name;        
         }
     }
 
@@ -83,21 +236,11 @@ class Apply_Sell_ExchangeShare extends Component
         $this->cust = Customer::where('icno', $user->icno)->first();
         $this->banks = RefBank::where('coop_id', $user->coop_id)->get();
 
-        $this->share = share::where('cust_id', $this->cust->id)->firstOrCreate([
-            'coop_id'     => $this->cust->coop_id, 
-            'cust_id'     => $this->cust->id, 
-            'amt_before'  => $this->cust->share,
-            'apply_amt'   => '0.00',
-            'direction'   => 'sell',
-            'flag'        => 0, 
-            'step'        => 0
-        ]);
+        $this->contApplyMember($this->cust->id);
+        $this->contApplyCoop($this->cust->id);
 
-        $this->share_apply  = $this->share?->apply_amt;
-        $this->bank_account = $this->share?->bank_account;
-        $this->bank_name    = $this->bank_name === $this->share->bank_code ? 'selected' : '';
-
-        $this->restricApply($this->cust->id);
+        $this->restricApplySell($this->cust->id);
+        $this->restricApplyExc($this->cust->id);
     }
 
     public function render()
