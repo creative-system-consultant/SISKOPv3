4f68e5540de075f723ecccecb596772eea861594
safwan <safwanazman017@gmail.com>
2022-07-21 15:16:39 +0800

    fix: layout of application list

--- resources/views/livewire/page/application/application-list/apply_contribution.blade.php
+++ resources/views/livewire/page/application/application-list/apply_contribution.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <div class="p-4">
     <h1 class="text-base font-semibold md:text-2xl">Apply Add Contribution</h1>
-    <div class="pt-4 mt-4 bg-white rounded-md shadow-md">
-        <div class="pl-4 pb-4 pr-4">
-            <h2 class="mb-4 mt-6 text-lg font-semibold border-b-2 border-gray-300">Applicant Information</h2>  
+    <x-general.card class="pt-4 mt-4 bg-white rounded-md shadow-md">
+        <div class="pb-4 pl-4 pr-4">
+            <h2 class="mt-6 mb-4 text-lg font-semibold border-b-2 border-gray-300">Applicant Information</h2>  
             <div class="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
                 <x-form.input 
                     label="Name" 
@@ -48,7 +48,7 @@
                 />                                
             </div>
 
-            <h2 class="mb-4 mt-6 text-lg font-semibold border-b-2 border-gray-300">Contribution Information</h2>  
+            <h2 class="mt-6 mb-4 text-lg font-semibold border-b-2 border-gray-300">Contribution Information</h2>  
             <div class="grid grid-cols-12 gap-6" x-data="{showing: '{{ $custApply->start_apply != NULL ? 'Starting Date' : 'One Month' }}', isSelect: false}">
                 <div class="col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 xl:col-span-4">
                     <x-form.input 
@@ -91,7 +91,7 @@
                 </div>
             </div>
 
-            <div class="p-4 mt-6 rounded-md bg-gray-50">
+            <div class="p-4 mt-6 rounded-md bg-gray-50 dark:bg-gray-600">
                 <div class="flex items-center justify-center space-x-2">
                     <a href="{{route('application.list')}}" class="flex items-center justify-center p-2 text-sm font-semibold text-white bg-red-500 border-2 rounded-md focus:outline-non">
                         Close
@@ -99,6 +99,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </x-general.card>
 </div>
 @endsection
