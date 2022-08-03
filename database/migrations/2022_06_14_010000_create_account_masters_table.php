2ec6439289de17fd637fe2c8367db38e01b62d0b
Aizuddin Yusoff <aizuddin@csc.net.my>
2022-08-03 11:46:11 +0800

    updated account master

--- database/migrations/2022_06_14_010000_create_account_masters_table.php
+++ database/migrations/2022_06_14_010000_create_account_masters_table.php
@@ -17,21 +17,38 @@ return new class extends Migration
             $table->id();
             $table->uuid('uuid')->default(DB::raw('newid()'));
 
-            $table->string('mbr_no');
+            $table->string('mbr_no')->nullable();
             $table->bigInteger('agent_id')->nullable();
+            $table->bigInteger('officer_id')->nullable();
             $table->bigInteger('cust_id');
             $table->bigInteger('coop_id');
 
-            $table->string('account_no');
+            $table->string('account_no')->nullable();
             $table->string('product_id');
             $table->bigInteger('account_status')->default('15');
 
-            $table->decimal('purchase_price',16,2);
-            $table->decimal('selling_price',16,2);
-            $table->decimal('approved_limit',16,2);
+            $table->decimal('apply_step',2,0)->default('0');
 
-            $table->decimal('profit_rate',4,4);
-            $table->decimal('instal_amount',16,4);
+            $table->decimal('purchase_price',16,2);
+            $table->decimal('selling_price',16,2)->nullable();
+            $table->decimal('approved_limit',16,2)->nullable();
+
+            $table->decimal('profit_rate',8,4);
+            $table->decimal('instal_amount',18,4)->nullable();
+            $table->decimal('instal_SI',18,4)->nullable();
+            $table->decimal('instal_Autopay',18,4)->nullable();
+            $table->decimal('total_instal_amount',18,4)->nullable();
+            $table->decimal('duration',2,0)->default('1');
+            $table->decimal('approved_duration',2,0)->default('1');
+
+            $table->string('cancel_tag')->nullable();
+            $table->string('cancel_desc')->nullable();
+            $table->timestamp('cancel_at')->nullable();
+            $table->string('cancel_by')->nullable();
+
+            $table->biginteger('referer_id')->nullable();
+            $table->decimal('referer_pay',16,2)->nullable();
+            $table->decimal('referer_pay_percent',5,2)->nullable();
 
             $table->timestamp('created_at')->useCurrent();
             $table->string('created_by')->nullable()->default('SYSTEM');
