a34ee9dfd89409bc3d2199a35f882174e58e0937
Abu <byedough11@gmail.com>
2022-08-23 15:09:19 +0800

    Guarantor Table

---
+++ database/migrations/2022_08_23_121535_create_guarantors_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siskop.guarantors', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->default(DB::raw('newid()'));
+            $table->bigInteger('coop_id')->nullable();
+            $table->morphs('guarantee');
+
+            $table->bigInteger('guarantor_cust_id')->nullable();
+            $table->decimal('commission',12,2)->nullable();
+            $table->decimal('comm_percent',5,2)->nullable();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->string('created_by')->nullable()->default('SYSTEM');
+            $table->timestamp('deleted_at')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->string('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siskop.guarantors');
+    }
+};
