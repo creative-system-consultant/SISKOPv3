e69f38029b2f48eda4864855468365877af01705
Aizuddin Yusoff <aizuddin@csc.net.my>
2023-11-27 18:12:31 +0800

    laravel permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,11 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
+        // Laravel Permission Middleware
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+
         // Custom Middleware
         'mustselectclient' => \App\Http\Middleware\MustSelectClient::class,
 
