6da718c18539743c0becdb0b39b223ef10482a27
Mehmed <iskandar.rahman@live.iium.edu.my>
2024-01-15 16:16:02 +0800

    changeGuarantor

--- app/Models/ChangeGuarantor.php
+++ app/Models/ChangeGuarantor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Traits\HasApprovals;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,7 +11,7 @@ class ChangeGuarantor extends Model
 {
     use HasFactory;
     use SoftDeletes;
-
+    use HasApprovals;
     protected $table = 'SISKOP.CHANGE_GUARANTOR';
     protected $guarded = [];
     protected $dates = ['created_at', 'deleted_at', 'updated_at'];
