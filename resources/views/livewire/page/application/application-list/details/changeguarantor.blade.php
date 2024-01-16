de48b194f1ca42d966056737d17751d428abafc4
wan <47422904+safwanAzman@users.noreply.github.com>
2024-01-16 17:50:19 +0800

    fix:change guarantor layout

--- resources/views/livewire/page/application/application-list/details/changeguarantor.blade.php
+++ resources/views/livewire/page/application/application-list/details/changeguarantor.blade.php
@@ -24,41 +24,72 @@
             </div>
 
             <h2 class="mt-6 mb-4 text-lg font-semibold border-b-2 border-gray-300">Change Guarantor Information</h2>
-            <div class="grid grid-cols-4 gap-2">
-                @foreach($ChangeGuarantorsDetails as $item)
-                    <x-form.input
-                        label="{{$loop->iteration}}. Old Guarantor Name"
-                        name=""
-                        value="{{$item->old_jamin_name}}"
-                        mandatory=""
-                        disable="true"
-                        type="text"
-                    />
-                    <x-form.input
-                        label="{{$loop->iteration}}. Old Guarantor Identity Number"
-                        name=""
-                        value="{{$item->old_jamin_icno}}"
-                        mandatory=""
-                        disable="true"
-                        type="text"
-                    />
-                    <x-form.input
-                        label="{{$loop->iteration}}. New Guarantor Name"
-                        name=""
-                        value="{{$item->new_jamin_name}}"
-                        mandatory=""
-                        disable="true"
-                        type="text"
-                    />
-                    <x-form.input
-                        label="{{$loop->iteration}}. New Guarantor Identity Number"
-                        name=""
-                        value="{{$item->new_jamin_icno}}"
-                        mandatory=""
-                        disable="true"
-                        type="text"
-                    />
-                @endforeach
+            <div class="mt-2">
+                <x-table.table>
+                    <x-slot name="thead">
+                        <tr  class="">
+                            <th  colspan="3" class="px-6 py-3 border  text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-center bg-primary-50" >
+                                Old Guarantor 
+                            </th>
+                            <th  colspan="3" class="px-6 py-3 border text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-center bg-primary-100" >
+                                New Guarantor
+                            </th>
+                        </tr>
+                        <tr>
+                            <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                                No
+                            </th>
+                            <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                                Name
+                            </th>
+                            <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                                NRIC
+                            </th>
+                            <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                                No
+                            </th>
+                            <th  colspan="1" class="px-6 py-3 border bg-gray-100 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                                Name
+                            </th>
+                            <th  colspan="1" class="px-6 py-3 border bg-gray-100 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                                NRIC
+                            </th>
+                        </tr>
+                        
+                    </x-slot>
+                    <x-slot name="tbody">
+                        @forelse($ChangeGuarantorsDetails as $item)
+                            <tr>
+                                <td  width="5%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                                    {{$loop->iteration}}
+                                </td>
+                                <td  width="30%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                                    {{$item->old_jamin_name}}
+                                </td>
+                                <td  width="15%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                                    {{$item->old_jamin_icno}}
+                                </td>
+
+                                <td  width="5%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                                    {{$loop->iteration}}
+                                </td>
+
+                                <td  width="35%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                                    {{$item->new_jamin_name}}
+                                </td>
+                                <td  width="15%" class="px-6 py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                                    {{$item->new_jamin_icno}}
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <x-table.table-body colspan="6" class="text-center">
+                                    No Data
+                                </x-table.table-body>
+                            </tr>
+                        @endforelse
+                    </x-slot>
+                </x-table.table>
             </div>
             <h2 class="mt-6 mb-4 text-lg font-semibold border-b-2 border-gray-300">Approvals</h2>
             <x-table.table>
@@ -73,9 +104,6 @@
                 <x-slot name="tbody">
                 @foreach ($Change->approvals as $item)
                     <tr>
-                        
-            
-
                         <x-table.table-body colspan="" class="text-left">
                             @if($item->order < $Change->step || $Change->flag > 1)
                                 <x-heroicon-o-check-circle class="w-6 h-6 text-green-500"/>
--- resources/views/livewire/page/executive/approval/approval/ChangeGuarantor.blade.php
+++ resources/views/livewire/page/executive/approval/approval/ChangeGuarantor.blade.php
@@ -1,49 +1,72 @@
 <div>
-    <h2 class="mt-6 mb-4 text-lg font-semibold border-b-2 border-gray-300">Change Guarantor Information</h2>
-    @foreach ($Application->details as $item)
-        <div class="grid grid-cols-1 lg:grid-cols-4 gap-2 space-y-4 lg:items-end">
-            <x-form.input-tag
-                label="{{$loop->iteration}}. Old Guarantor Name"
-                type="text"
-                name=""
-                value="{{ $item->old_jamin_name  }}"
-                leftTag=""
-                rightTag=""
-                mandatory=""
-                disable="true"
-            />
-            <x-form.input-tag
-                label="{{$loop->iteration}}. Old Guarantor Identity No"
-                type="text"
-                name=""
-                value="{{ $item->old_jamin_icno  }}"
-                leftTag=""
-                rightTag=""
-                mandatory=""
-                disable="true"
-            />
-            <x-form.input-tag
-                label="{{$loop->iteration}}. New Guarantor Name"
-                type="text"
-                name=""
-                value="{{ $item->new_jamin_name  }}"
-                leftTag=""
-                rightTag=""
-                mandatory=""
-                disable="true"
-            />
-            <x-form.input-tag
-                label="{{$loop->iteration}}. New Guarantor Identity No"
-                type="text"
-                name=""
-                value="{{ $item->new_jamin_icno  }}"
-                leftTag=""
-                rightTag=""
-                mandatory=""
-                disable="true"
-            />
-        </div>
-    @endforeach
+    <h2 class="mt-6 mb-4 text-lg font-semibold border-b-2 border-gray-300">Special Aid Information</h2>
+    <div class="mt-2">
+        <x-table.table>
+            <x-slot name="thead">
+                <tr  class="">
+                    <th  colspan="3" class="px-6 py-3 border  text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-center bg-primary-50" >
+                        Old Guarantor 
+                    </th>
+                    <th  colspan="3" class="px-6 py-3 border text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-center bg-primary-100" >
+                        New Guarantor
+                    </th>
+                </tr>
+                <tr>
+                    <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                        No
+                    </th>
+                    <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                        Name
+                    </th>
+                    <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                        NRIC
+                    </th>
+                    <th  colspan="1" class="px-6 py-3 border bg-gray-50 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                        No
+                    </th>
+                    <th  colspan="1" class="px-6 py-3 border bg-gray-100 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                        Name
+                    </th>
+                    <th  colspan="1" class="px-6 py-3 border bg-gray-100 text-xs leading-4 font-medium uppercase tracking-wider dark:bg-gray-600 dark:text-white text-left" >
+                        NRIC
+                    </th>
+                </tr>
+                
+            </x-slot>
+            <x-slot name="tbody">
+                @forelse($Application->details as $item)
+                    <tr>
+                        <td  width="5%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                            {{$loop->iteration}}
+                        </td>
+                        <td  width="30%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                            {{$item->old_jamin_name}}
+                        </td>
+                        <td  width="15%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                            {{$item->old_jamin_icno}}
+                        </td>
+
+                        <td  width="5%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                            {{$loop->iteration}}
+                        </td>
+
+                        <td  width="35%" class="px-6  py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                            {{$item->new_jamin_name}}
+                        </td>
+                        <td  width="15%" class="px-6 py-2 whitespace-no-wrap leading-5 bg-white dark:bg-gray-700 dark:text-white border text-left text-xs">
+                            {{$item->new_jamin_icno}}
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <x-table.table-body colspan="6" class="text-center">
+                            No Data
+                        </x-table.table-body>
+                    </tr>
+                @endforelse
+            </x-slot>
+        </x-table.table>
+    </div>
 
 </div>
 
