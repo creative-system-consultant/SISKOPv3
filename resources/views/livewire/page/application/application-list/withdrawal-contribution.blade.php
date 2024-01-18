5831a24f749454ebd8841a42dae66cca568bd4ad
MnamesJeff <iskandar.rahman@iium.edu.my>
2024-01-18 15:39:15 +0800

    gitflow-feature-stash: ApplicationListApprovalStatus

--- resources/views/livewire/page/application/application-list/withdrawal-contribution.blade.php
+++ resources/views/livewire/page/application/application-list/withdrawal-contribution.blade.php
@@ -45,8 +45,8 @@
                     <x-table.table-body colspan="" class="text-left uppercase">
                         @if ($item->flag == '0') Still being applied
                         @elseif ($item->flag == '1') Being Processed
-                        @elseif ($item->flag == '3') Failed / Decline
-                        @elseif ($item->flag == '6') Approved
+                        @elseif ($item->flag == '21') Failed / Decline
+                        @elseif ($item->flag == '20') Approved
                         @endif
                     </x-table.table-body>
                     <x-table.table-body colspan="" class="text-left">
