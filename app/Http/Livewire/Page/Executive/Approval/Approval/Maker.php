78704853e459ff1b7863e01f9c57a6e95e557cc6
hafidz <hafidz@csc.net.my>
2024-03-14 11:26:34 +0800

    DividendWithdrawal
    fix approved amount

--- app/Http/Livewire/Page/Executive/Approval/Approval/Maker.php
+++ app/Http/Livewire/Page/Executive/Approval/Approval/Maker.php
@@ -44,6 +44,7 @@ class Maker extends Component
     public $guarantorLists;
     public $jaminan;
     public $events_date;
+    public $div_cash_approved,$div_share_approved,$div_contri_approved;
 
     protected function rules()
     {
@@ -222,7 +223,8 @@ class Maker extends Component
     }
 
     public function next()
-    {
+    {   
+        //dd($this->Application->div_cash_approved,$this->Application->div_share_approved,$this->Application->div_contri_approved);
         if ($this->approval_type != 'gagal') {
             $this->validate($this->xvalidate());
             $this->validate($this->shareValidation());
@@ -394,6 +396,11 @@ class Maker extends Component
             $this->Application->div_cash_approved = number_format($this->Application->div_cash_apply, 2);
             $this->Application->div_share_approved = number_format($this->Application->div_share_apply, 2);
             $this->Application->div_contri_approved = number_format($this->Application->div_contri_apply, 2);
+
+            // $this->Application->div_cash_approved = $this->div_cash_approved;
+            // $this->Application->div_share_approved = $this->div_share_approved;
+            // $this->Application->div_contri_approved = $this->div_contri_approved;
+           
         } else if ($this->include == 'specialaid') {
             $this->Application = ApplySpecialAid::where('uuid', $uuid)->where('client_id', $this->User->client_id)->with('customer')->first();
             $this->Application->approved_amt = $this->Application->approved_amt ?? $this->Application->apply_amt;
